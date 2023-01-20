31b2a747e326ce9701b57d4394ca44acab5cc6fb
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-21 00:26:03 +0530

    [Sankalp]- Create a Queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,9 +6,10 @@
         {
             Console.WriteLine("\t\t\t\t\tData Structures : Stacks and Queues");
             Console.WriteLine();
-            Console.WriteLine("-------------Stack Operations-------------");
+            Console.WriteLine("-------------Stack and Queue Operations-------------");
             Console.WriteLine("1.Push operation");
             Console.WriteLine("2. Peek and Pop till the stack is empty");
+            Console.WriteLine("3. Create Queue of 56->30->70");
             Console.WriteLine("------------------------------------------");
             Console.Write("Enter the option : ");
             int num = Convert.ToInt32(Console.ReadLine());
@@ -30,6 +31,16 @@
                     linkedListStack.Pop();
                     linkedListStack.IsEmpty();
                     linkedListStack.Display();
+                    break;
+                case 3:
+                    LinkedListQueue linkedListQueue = new LinkedListQueue();
+                    linkedListQueue.Enqueue(56);
+                    linkedListQueue.Enqueue(30);
+                    linkedListQueue.Enqueue(70);
+                    linkedListQueue.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
                     break;
             }
         }
