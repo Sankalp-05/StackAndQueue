ede7f872dfaaf2c9a4a322adaa0fff952f390dae
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-21 00:13:24 +0530

    [Sankalp]- Create a Stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Program ");
+            Console.WriteLine("\t\t\t\t\tData Structures : Stacks and Queues");
+            Console.WriteLine();
+            Console.WriteLine("-------------Stack Operations-------------");
+            Console.WriteLine("1.Push operation");
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Enter the option : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            LinkedListStack linkedListStack = new LinkedListStack();
+            switch (num)
+            {
+                case 1:
+                    linkedListStack.Push(70);
+                    linkedListStack.Push(30);
+                    linkedListStack.Push(56);
+                    linkedListStack.Display();
+                    break;
+            }
         }
     }
 }
